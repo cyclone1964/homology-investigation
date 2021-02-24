d4218221191e76b7cd1945b4cf9ed6ff70c69011
Matthew Daily <matt.daily@cox.net>
2021-02-24 12:38:57 -0500

    Updated for nishaq's latest implementation and to conform with new software layout

--- slayer-investigation/DataGenerator/generateClamBarcodes.py
+++ slayer-investigation/DataGenerator/generateClamBarcodes.py
@@ -1,84 +1,142 @@
+import os
 import sys
-sys.path.append('/home/mdaily/Software/PhD/clam')
-sys.path.append('/home/mdaily/Software/PhD/objrec')
+sys.path.append('/Users/Matt/Documents/URI/clam')
+sys.path.append('/Users/Matt/Documents/URI/objrec')
 
+import heapq
+import numpy as np
 from typing import Dict
 from typing import List
-from typing import Set
 from typing import Tuple
+from typing import Optional
 
 from pyclam import Cluster
 from pyclam import Manifold
-
-from src.utils import *
+from pyclam import criterion
 
 # Cluster -> (birth-radius, death-radius)
 Barcodes = Dict[Cluster, Tuple[float, float]]
 
-def create_barcodes(
-        data: np.array,
-        *,
-        steps: Optional[int] = 10**3,
-        normalize: bool = False,
-        merge: Optional[int] = None,
-) -> Dict[int, Barcodes]:
-    manifold: Manifold = Manifold(data, 'euclidean').build()
-    thresholds: np.array = np.linspace(start=manifold.root.radius * (steps - 1) / steps, stop=0, num=steps)
-    barcodes: Barcodes = dict()
-    living_clusters: Barcodes = {manifold.root: (-1, manifold.root.radius)}
-    for threshold in thresholds:
-        new_births: Set[Cluster] = set()
-        dead_clusters: Set[Cluster] = {cluster for cluster in living_clusters if cluster.radius > threshold}
-        while dead_clusters:
-            cluster = dead_clusters.pop()
-            death = living_clusters.pop(cluster)[1] if cluster in living_clusters else threshold
-            barcodes[cluster] = threshold, death
-            for child in cluster.children:
-                if child.cardinality > 1:
-                    (dead_clusters if child.radius > threshold else new_births).add(child)
-                else:
-                    barcodes[child] = (0, threshold)
-        living_clusters.update({cluster: (-1, threshold) for cluster in new_births})
+class Code:
+    def __init__(self, cluster: Cluster, death: float, birth: float = -1):
+        self._cluster: Cluster = cluster
+        self._death: float = death
+        self._birth: float = birth
+        return
 
-    if normalize:  # normalize radii to [0, 1] range.
-        factor = manifold.root.radius
-        barcodes = {c: (b / factor, d / factor) for c, (b, d) in barcodes.items()}
-    
+    def set_birth(self, birth: float):
+        self._birth = birth
+        return
+
+    @property
+    def cluster(self) -> Cluster:
+        return self._cluster
+
+    @property
+    def death(self) -> float:
+        return self._death
+
+    @property
+    def birth(self) -> float:
+        return self._birth
+
+    @property
+    def radius(self) -> float:
+        return self._cluster.radius
+
+    def __lt__(self, other: 'Code'):
+        return self.cluster.radius > other.cluster.radius
+
+
+def _normalize(factor: float, barcodes: Barcodes) -> Barcodes:
+    return {c: (b / factor, d / factor) for c, (b, d) in barcodes.items()}
+
+
+def _group_by_cardinality(barcodes: Barcodes) -> Dict[int, Barcodes]:
     cardinalities: List[int] = list(sorted({cluster.cardinality for cluster in barcodes}))
     barcodes_by_cardinality: Dict[int, Barcodes] = {cardinality: dict() for cardinality in cardinalities}
     [barcodes_by_cardinality[cluster.cardinality].update({cluster: lifetime})
      for cluster, lifetime in barcodes.items()]
-    
-    if merge is not None:
-        # Merges all barcodes for clusters with cardinality greater than 'merge'
-        high_cardinalities = [v for c, v in barcodes_by_cardinality.items() if c >= merge]
-        if len(high_cardinalities) > 0:
-            [high_cardinalities[0].update(h) for h in high_cardinalities[1:]]
-            barcodes_by_cardinality = {c: v for c, v in barcodes_by_cardinality.items() if c < merge}
-            barcodes_by_cardinality[merge] = high_cardinalities[0]
-    
     return barcodes_by_cardinality
 
-def generateClamBarcodes(path,
-                         num_points: int = 10**2,
-                         resolution: int = 10**2,
-                         number_per_shape: int = 10**1):
-            
+
+def _merge_high_cardinalities(
+        threshold: int,
+        barcodes_by_cardinality: Dict[int, Barcodes],
+) -> Dict[int, Barcodes]:
+
+    # Merges all barcodes for clusters with cardinality greater than 'threshold'
+    high_cardinalities = [v for c, v in barcodes_by_cardinality.items() if c >= threshold]
+    if len(high_cardinalities) > 0:
+        [high_cardinalities[0].update(h) for h in high_cardinalities[1:]]
+        barcodes_by_cardinality = {c: v for c, v in barcodes_by_cardinality.items() if c < threshold}
+        barcodes_by_cardinality[threshold] = high_cardinalities[0]
+    return barcodes_by_cardinality
+
+
+def create_barcodes(
+        data: np.array,
+        *,
+        normalize: bool = True,
+        merge: Optional[int] = 4,
+) -> Dict[int, Barcodes]:
+    manifold: Manifold = Manifold(data, 'euclidean').build_tree(criterion.MaxDepth(20))
+    barcodes: Barcodes = dict()
+
+    # living-clusters is a heap with highest radius at the top
+    living_clusters = [Code(manifold.root, manifold.root.radius)]
+    heapq.heapify(living_clusters)
+
+    while living_clusters:  # Go over max-heap
+        current: Code = heapq.heappop(living_clusters)
+
+        if current.cluster.children:  # handle children
+            current.set_birth(current.radius)
+            [left, right] = list(current.cluster.children)
+
+            if left.radius >= current.radius:  # left is still-born
+                barcodes[left] = (current.radius, current.radius)
+            else:  # or added to living clusters
+                heapq.heappush(living_clusters, Code(left, current.radius))
+
+            if right.radius >= current.radius:  # right is still-born
+                barcodes[right] = (current.radius, current.radius)
+            else:  # or added to living-clusters
+                heapq.heappush(living_clusters, Code(right, current.radius))
+
+        else:  # otherwise set birth to zero-radius
+            current.set_birth(0.)
+        # add current to dict of barcodes
+        barcodes[current.cluster] = (current.birth, current.death)
+
+    if normalize:
+        barcodes = _normalize(manifold.root.radius, barcodes)
+
+    barcodes_by_cardinality = _group_by_cardinality(barcodes)
+
+    if merge is not None:
+        barcodes_by_cardinality = _merge_high_cardinalities(merge, barcodes_by_cardinality)
+
+    return barcodes_by_cardinality
+
+def generateBarcodes(inputPath,
+                     outputPath):
     # Read the labels to find out how many shapes there are
-    labels = np.loadtxt(os.path.join(path,'Labels.dat'))
+    labels = np.loadtxt(os.path.join(inputPath,'Labels.dat'))
     num_shapes = len(labels)
     for index in range(num_shapes):
-        filename = os.path.join(path,f'Shape{index}.dat')
+        inputFile = os.path.join(inputPath,f'Shape{index}.dat')
         if (index%100 == 0):
-            print(filename)
-        shape = np.loadtxt(filename,
+            print(inputFile)
+        shape = np.loadtxt(inputFile,
                            delimiter=',',
                            ndmin=2)
-        filename = os.path.join(path,f'Shape{index}.bc')
-        with open(filename, 'w') as fp:
+        outputFile = os.path.join(outputPath,f'Shape{index}.bc')
+        if (count%100 == 0):
+            print('  Create Clam Barcode: ',outputFile)
+        with open(outputFile, 'w') as fp:
             barcodes_by_cardinality = create_barcodes(
                 shape,
-                steps=resolution,
                 normalize=True,
                 merge=4,
             )
@@ -86,23 +144,24 @@ def generateClamBarcodes(path,
             for cardinality in cardinalities:
                 barcodes = barcodes_by_cardinality[cardinality]
                 barcodes = list(sorted([(birth, death) for birth, death in barcodes.values()]))
-                with open(filename, 'a') as fp:
-                    for birth, death in barcodes:
-                        fp.write(f'{birth:.4f},{death:.4f},{cardinality}\n')
+                for birth, death in barcodes:
+                    fp.write(f'{birth:.4f},{death:.4f},{cardinality}\n')
         fp.close()
 
         # Now load the barcodes and make the sli from them
-        bc = np.loadtxt(filename,delimiter=',',ndmin=2)
+        bc = np.loadtxt(outputFile,delimiter=',',ndmin=2)
         
         persistence = bc[:,1] - bc[:,0]
         dims = bc[:,2].astype(int)
-    
+
+        np.savetxt(os.path.join(outputPath,'Labels.txt'),labels);
+        
         for dim in np.unique(dims):
             indices = np.nonzero(dims == dim)
             indices = indices[0]
 
             i = np.argsort(persistence[indices])
             indices = indices[i]
-            np.savetxt("{}/Shape{}_dim_{}.sli".format(path,index,dim),
+            np.savetxt("{}/Shape{}_dim_{}.sli".format(outputPath,index,dim-1),
                        bc[indices,0:2])
 
